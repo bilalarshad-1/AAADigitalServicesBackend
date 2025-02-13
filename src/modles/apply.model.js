a4084ae57746623ed397eed840a5346b105f4ef5
bilalarshad-1 <bilalarshad03054@gmail.com>
2025-02-13 17:57:56 +0500

    builds image url is not more

--- src/modles/apply.model.js
+++ src/modles/apply.model.js
@@ -4,6 +4,63 @@ import { Schema } from "mongoose";
 const applySchema = new Schema(
     
     {
+    fullname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: Number,
+        required: true
+    },
+    applyingfor: {
+        type: String,
+        required: true
+    },
+    experience: {
+        type: String,
+        required: true
+    },
+    positionlevel: {
+        type: String,
+        required: true
+    },
+    joiningavailability: {
+        type: String,
+        required: true
+    },
+    jobtype: {
+        type: String,
+        required: true
+    },
+    cv: {
+
+    },
+    linkedinprofile: {
+        type: String,
+        required: true
+    },
+    currentcompany: {
+        type: String,
+        required: true
+    },
+    graduatedfrom: {
+        type: String,
+        required: true
+    },
+    currentsalary: {
+        type: Number,
+        required: true
+    },
+    expectedsalary: {
+        type: Number,
+        required: true
+    },
+
+
 
     },
     
